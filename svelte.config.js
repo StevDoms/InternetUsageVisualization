c4830547db5de021407bc423c075e8d1fd7a6cf8
StevDoms <stevdoms@gmail.com>
2024-02-19 21:47:29 -0800

    Config

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,6 @@ const config = {
         adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			precompress: false,
-			strict: true
 		}),
 		paths: {
             base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
