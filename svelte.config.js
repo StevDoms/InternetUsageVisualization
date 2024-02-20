2c9444ab74588cb674af013bc359392e7a90ba43
StevDoms <stevdoms@gmail.com>
2024-02-19 20:41:03 -0800

    Config

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
+		}),
 		paths: {
             base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
         }
