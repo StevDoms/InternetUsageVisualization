503a8dc593e9e274a7ef70fac502923aafb9cd3d
StevDoms <stevdoms@gmail.com>
2024-02-19 21:50:55 -0800

    404

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ const config = {
         adapter: adapter({
 			pages: 'build',
 			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
             base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
